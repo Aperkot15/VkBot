a0ce639579104b6badc8fe75379a81242572b7d1
Arthur <a.velesnitskiya@gmail.com>
2020-09-09 11:23:35 +0300

    v1

---
+++ Controllers/BotController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vkbot.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BotController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("vk bot!");
+        }
+        [HttpPost("callback")]
+        public IActionResult CallBack(JsonElement data) 
+        {
+            
+            return Ok();
+        }
+        [HttpGet("callback")]
+        public IActionResult CallBackGet()
+        {
+            return Ok("Callback test!");
+        }
+    }
+}
